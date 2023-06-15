da8c54148ebb6947548a3d646662989048e5ba57
Lukas Gonzalez Ortega <lukas@vm.inception>
2023-06-15 19:55:58 +0000

    CHORE: inception v1234^1234

---
+++ Makefile
@@ -0,0 +1,24 @@
+COMPOSE=docker compose -f srcs/docker-compose.yml
+
+.PHONY: all build follow logs ps
+
+all:
+	$(COMPOSE) up -d
+
+down:
+	$(COMPOSE) down
+
+build:
+	$(COMPOSE) build --no-cache
+
+logs:
+	$(COMPOSE) logs
+
+follow:
+	$(COMPOSE) logs --follow
+
+ps:
+	$(COMPOSE) ps
+
+edit:
+	vim srcs/docker-compose.yml
---
+++ srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+mariadb-install-db --user=mysql --verbose --ldata=/var/lib/mysql
+
+exec mysqld_safe --user=mysql --log-error=/log/mariadb/err.log &
+
+MYQSL_PID=$!
+
+while ! mysqladmin ping >/dev/null 2>&1; do
+	sleep 1
+done
+
+if [ ! -f /log/mariadb ]; then
+	mkdir -p /log/mariadb
+fi
+
+chown -R mysql:mysql /log/mariadb
+
+mysql -u root -pmyadmin_password <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost';
+    FLUSH PRIVILEGES;
+EOSQL
+
+wait $MYQSL_PID
