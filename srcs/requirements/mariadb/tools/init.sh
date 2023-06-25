e0cf09e3dcb5747994f26ce5d7dbbf238514171e
lugonzalg <glukas94@gmail.com>
2023-06-25 19:11:14 +0200

    AutomaticGit  Sun 25 Jun 19:11:14 CEST 2023

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 
+#SOURCE -> https://github.com/yobasystems/alpine-mariadb/blob/master/alpine-mariadb-aarch64/files/run.sh
+
+#CHECK if daemon exists
+if [ -f /run/mysqld ]; then
+	echo "mysqld already exists, setting permissions"
+	chown -R mysql:mysql /run/mysqld
+else
+	echo "mysqld doesn't exist! Creating ..."
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
+
+#CHECK if database exists
+if [ -d /var/lib/mysql/mysql ]; then
+	echo "mysql already exists, setting permissions"
+	chown -R /var/lib/mysql
+else
+	echo "mysql doesn't exist! Creating ..."
+	mkdir -p /var/lib/mysql
+	chown -R /var/lib/mysql
+fi
+
 mariadb-install-db --user=mysql --verbose --ldata=/var/lib/mysql
 
 exec mysqld_safe --user=mysql --log-error=/log/mariadb/err.log &
