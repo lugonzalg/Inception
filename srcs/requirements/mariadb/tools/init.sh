c820a9e9e2ca4455d2e96cd180b6749d60675e3b
lugonzalg <glukas94@gmail.com>
2023-06-25 19:41:04 +0200

    CHORE: mariadb init.sh file modified - sourced from the original script

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -4,43 +4,37 @@
 
 #CHECK if daemon exists
 if [ -f /run/mysqld ]; then
-	echo "mysqld already exists, setting permissions"
+	echo "mysqld daemon already exists, setting permissions"
 	chown -R mysql:mysql /run/mysqld
 else
-	echo "mysqld doesn't exist! Creating ..."
+	echo "mysqld daemon doesn't exist! Creating ..."
 	mkdir -p /run/mysqld
 	chown -R mysql:mysql /run/mysqld
 fi
 
 #CHECK if database exists
 if [ -d /var/lib/mysql/mysql ]; then
-	echo "mysql already exists, setting permissions"
-	chown -R /var/lib/mysql
+	echo "mysql database already exists, setting permissions"
+	chown -R mysql:mysql /var/lib/mysql
 else
-	echo "mysql doesn't exist! Creating ..."
-	mkdir -p /var/lib/mysql
-	chown -R /var/lib/mysql
+	echo "mysql database doesn't exist! Creating ..."
+	chown -R mysql:mysql /var/lib/mysql
+
+	mariadb-install-db --user=mysql --verbose --ldata=/var/lib/mysql
+
+	tmp_file=mktemp
+
+	cat << EOF > $tmp_file
+	CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+	GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost';
+	FLUSH PRIVILEGES;
+EOF
+
+	/usr/bin/mysqld --user=mysql --bootstrap --skip-name-resolve --skip-networking=0 < $tfile
+
 fi
 
-mariadb-install-db --user=mysql --verbose --ldata=/var/lib/mysql
-
-exec mysqld_safe --user=mysql --log-error=/log/mariadb/err.log &
-
-while ! mysqladmin ping >/dev/null 2>&1; do
-	sleep 1
-done
-
-if [ ! -f /log/mariadb ]; then
-	mkdir -p /log/mariadb
-fi
-
-chown -R mysql:mysql /log/mariadb
-
-mysql -u root -pmyadmin_password <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
-    GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost';
-    FLUSH PRIVILEGES;
-EOSQL
+/usr/bin/mysqld --user=mysql --skip-name-resolve --skip-networking=0 --silent-startup &
 
 exec /usr/bin/mysqld --user=mysql --log-error=/log/mariadb/err.log --console --skip-name-resolve --skip-networking=0 $@
